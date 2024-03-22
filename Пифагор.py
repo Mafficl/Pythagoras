69cdc679c0ff7b7106261b1ed715c15fbac63593
Mafficl <164522610+Mafficl@users.noreply.github.com>
2024-03-22 20:15:38 +0300

    Add files via upload

---
+++ Пифагор.py
@@ -0,0 +1,17 @@
+from math import sqrt
+print ("Теореама Пифагора")
+print ('Если нужно найти катет нажмите 1', 'Если нужно найти гипотенузу нажмите 2', sep='\n')
+x = input()
+if x==1:
+    a = input('а = ')
+    c = input('c = ')
+    if a<c:
+         b = sqrt((c**2-a**2))
+         print("Катет = ", b)
+    else:
+         print("Такого треугольника не существует")
+if x==2:
+     a = input('а = ')
+     b = input('b = ')   
+     c = sqrt((a**2+b**2))
+     print("Гипотенуза =", c)
